f2d989fbd525b4aa602f7c51e0b2d558504b781e
Anders Hafreager <anders@tapcat.is>
2012-11-11 18:00:32 +0100

    Added user activity support

--- cpu-usage-reporter.py
+++ cpu-usage-reporter.py
@@ -13,6 +13,7 @@ import time
 import random
 import os
 import ConfigParser
+from xlib import XEvents
 from sys import argv
 
 # UPDATE THESE VARIABLES
@@ -27,7 +28,26 @@ def loadUserNameFromFile():
         if configParser.has_option("General", "username"):
             username = configParser.get("General", "username")
             print("Updating username to", username)
-    
+
+def getEventCount():
+    while not events.listening():
+        # Wait for init
+        time.sleep(1)
+    try:
+        clicks_past_period = 0
+        while events.listening():
+            evt = events.next_event()
+            if not evt:
+                return clicks_past_period
+            
+            if evt.type != "EV_KEY" or evt.value != 1: # Only count key down, not up.
+                continue
+
+            clicks_past_period+=1
+    except:
+        print "Caught exception, you are a bad boy"
+
+    return 0
 
 configPath = "/etc/cpu-usage-reporter.conf"
 username = "unnamed" + ("%.0f" % (random.random() * 100))
@@ -44,17 +64,27 @@ print("Using username " + username)
 
 samples = 1
 usageSum = 0
+
+events = XEvents()
+events.start()
+
 while(True):
     cpuUsage = psutil.cpu_percent()
     usageSum += cpuUsage
+    
     if samples > 11:
+        eventCount = getEventCount()
+
+        print "Events: "+str(eventCount)
         if os.path.exists(configPath):
             loadUserNameFromFile()
         averageUsage = float(usageSum) / float(samples)
         print("Pushing usage to server", averageUsage)
         try:
-            runData = json.load(urllib2.urlopen(baseUrl + "/wp-content/plugins/cpu-reporter/submit.php?user=" + username + "&usage=" + str(averageUsage)))
+            url = baseUrl + "/wp-content/plugins/cpu-reporter/submit.php?user=" + username + "&usage=" + str(averageUsage)+ "&is_active=" + str(eventCount)
+            runData = json.load(urllib2.urlopen(url))
         except KeyboardInterrupt:
+            events.stop_listening()
             raise KeyboardInterrupt
         except:
             print("Something bad happened. Don't care...")
